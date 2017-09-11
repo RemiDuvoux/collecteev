44b26d70bb7b61e1dd596c0fa3c1385a2ce320f7
Remi Duvoux <remi.duvoux@gmail.com>
2017-09-11 18:19:45 +0200

    update location of mangopay logfile

--- config/initializers/mangopay.rb
+++ config/initializers/mangopay.rb
@@ -2,6 +2,6 @@ MangoPay.configure do |c|
   c.preproduction = true
   c.client_id = ENV['MANGOPAY_CLIENT_ID']
   c.client_passphrase = ENV['MANGOPAY_SANDBOX_PASSPHRASE']
-  c.log_file = File.join('mypath', 'mangopay.log')
+  c.log_file = Rails.root.join('mangopay.log').to_s
   c.http_timeout = 10000
 end
